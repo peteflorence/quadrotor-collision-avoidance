a429b7fe4b19a40c26d7d3f6af7b3a985441063a
peteflorence <pete.florence@gmail.com>
2016-06-17 13:00:26 -0400

    drift time formulation works okay up to about 8 m/s

--- src/trajectory.cpp
+++ src/trajectory.cpp
@@ -43,11 +43,12 @@ Vector3 Trajectory::getTerminalStopPosition(Scalar const& t) const {
 
   double speed = velocity_end_of_trajectory.norm();
   double stop_t = speed / a_max_horizontal;
+  double drift_time = 0.300;
 
   //double stopping_distance = 0.5 * -a_max_horizontal * stop_t*stop_t +speed*stop_t;
   //return position_end_of_trajectory + stopping_distance*velocity_end_of_trajectory/speed;
 
-  return 0.5*a_max_horizontal*-(velocity_end_of_trajectory)/speed*stop_t*stop_t + velocity_end_of_trajectory*stop_t + position_end_of_trajectory;
+  return initial_velocity*drift_time + 0.5*(a_max_horizontal)*-(velocity_end_of_trajectory)/speed*stop_t*stop_t + velocity_end_of_trajectory*(stop_t+drift_time) + position_end_of_trajectory;
 
 }
 
