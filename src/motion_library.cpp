ba46ca7b7a3b5bbf45523ab244cfbf17c04d2b35
peteflorence <pete.florence@gmail.com>
2016-10-17 18:31:25 -0400

    more readable, still not fixed

--- src/motion_library.cpp
+++ src/motion_library.cpp
@@ -2,7 +2,7 @@
 
 void MotionLibrary::Initialize2DLibrary(double a_max_horizontal, double min_speed_at_max_acceleration_total, double max_acceleration_total) {
 
-    this->min_speed_at_max_acceleration_total = min_speed_at_max_acceleration_total;
+    this->speed_at_acceleration_max = min_speed_at_max_acceleration_total;
     this->max_acceleration_total = max_acceleration_total;
 	initial_max_acceleration = a_max_horizontal;
 	
@@ -44,21 +44,22 @@ void MotionLibrary::Initialize2DLibrary(double a_max_horizontal, double min_spee
 };
 
 void MotionLibrary::UpdateMaxAcceleration(double speed) {
-	double speed_truncated = speed;
 
-	if (speed_truncated > min_speed_at_max_acceleration_total) {
-		speed_truncated = min_speed_at_max_acceleration_total;
-	}
-
-	double added_scale_max_acceleration = speed_truncated/min_speed_at_max_acceleration_total * (max_acceleration_total - initial_max_acceleration)/initial_max_acceleration;
-
-	double scale_max_acceleration = 1.0 + added_scale_max_acceleration;
-	new_max_acceleration = initial_max_acceleration * scale_max_acceleration;
+	new_max_acceleration = ComputeNewMaxAcceleration(speed);
 
 	for (size_t index = 0; index < motions.size(); index++) {
 		motions.at(index).setAccelerationMax(new_max_acceleration);
-		motions.at(index).ScaleAcceleration(scale_max_acceleration);
+		motions.at(index).ScaleAcceleration(new_max_acceleration/initial_max_acceleration);
 	}
+}
+
+double MotionLibrary::ComputeNewMaxAcceleration(double speed) {
+	if (speed > speed_at_acceleration_max) {
+		return max_acceleration_total;
+	}
+
+	return speed * (max_acceleration_total - initial_max_acceleration) / speed_at_acceleration_max + initial_max_acceleration;
+
 }
 
 double MotionLibrary::getNewMaxAcceleration() const {
@@ -187,5 +188,5 @@ void MotionLibrary::setMaxAccelerationTotal(double max_accel) {
 }
 
 void MotionLibrary::setMinSpeedAtMaxAccelerationTotal(double speed) {
-  this->min_speed_at_max_acceleration_total = speed;
+  this->speed_at_acceleration_max = speed;
 }
