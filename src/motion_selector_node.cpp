9221e0ea6a495b2debcbb7f4a9c293fb165c77ce
peteflorence <pete.florence@gmail.com>
2016-10-31 21:16:17 -0400

    good settings for 4 m/s 3d flight through avon park

--- src/motion_selector_node.cpp
+++ src/motion_selector_node.cpp
@@ -722,7 +722,7 @@ private:
 	tf2_ros::Buffer tf_buffer_;
 
 	double start_time = 0.0;
-	double final_time = 1.0;
+	double final_time = 1.5;
 
 	double bearing_azimuth_degrees = 0.0;
 	double set_bearing_azimuth_degrees = 0.0;
